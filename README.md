e7e0e387b0c6e8d146f2b711f9228a751dec3352
James D <jamesgecko@users.noreply.github.com>
2023-12-02 13:39:17 +0800

    Day1-1

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # advent-2023
 
+`./gradlew jvmRun -DmainClass=Day01Kt --quiet`
+
 Welcome to the Advent of Code[^aoc] Kotlin project created by [jamesgecko][github] using the [Advent of Code Kotlin Template][template] delivered by JetBrains.
 
 In this repository, jamesgecko is about to provide solutions for the puzzles using [Kotlin][kotlin] language.
--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,18 @@
+import java.io.*
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val digits = input.map { line -> line.filter { it.isDigit() } }
+        val calibration = digits.map { "${it.first()}${it.last()}".toInt() }
+        return calibration.sum()
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day01_test1")
+    check(part1(testInput) == 142)
 
     val input = readInput("Day01")
     part1(input).println()
